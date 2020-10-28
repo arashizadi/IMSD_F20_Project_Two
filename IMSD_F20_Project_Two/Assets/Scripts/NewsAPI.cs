e64843ebfeadbbeeea830d010e441b2fde2c881c
Arash Izadi <arash.iz@live.com>
2020-10-27 18:44:34 -0700

    Updated NewsAPI.cs (not functional)

--- IMSD_F20_Project_Two/Assets/Scripts/NewsAPI.cs
+++ IMSD_F20_Project_Two/Assets/Scripts/NewsAPI.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Xml.Linq;
 
 public class NewsAPI : MonoBehaviour
 {
+    string apiReturn;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +20,28 @@ public class NewsAPI : MonoBehaviour
     {
         
     }
+    IEnumerator GetNews()
+    {
+        UnityWebRequest www = UnityWebRequest.
+            Get("http://api.openweathermap.org/data/2.5/weather?zip=" + "zip" + "&mode=xml&APPID=" + "key");
+        yield return www.SendWebRequest();
+        if (!www.isNetworkError && !www.isHttpError)
+        {
+            // Get text content like this:
+            Debug.Log(www.downloadHandler.text);
+            apiReturn = www.downloadHandler.text;
+        }
+        else
+        {
+            Debug.Log(www.error + " " + www);
+        }
+    }
 }
+
+    
+/*    {
+        XDocument doc = XDocument.Parse(apiReturn);
+        XAttribute _currentTemp = doc.Element("current").Element("temperature").Attribute("value");
+        currentTemp = Double.Parse(_currentTemp.Value);
+
+    }*/
