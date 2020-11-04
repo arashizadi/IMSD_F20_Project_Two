8a327e4d801ae885b109161a9ad469018f320f30
Arash Izadi <arash.iz@live.com>
2020-11-04 03:40:03 -0800

    Added New Level Functionality

---
+++ IMSD_F20_Project_Two/Assets/NewspaperScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewspaperScript : MonoBehaviour
+{
+    public Button factCheck;
+    public TMP_InputField userText;
+    public GameObject newspaperObject;
+
+    int level = 1;
+    void Start()
+    {
+        factCheck.onClick.AddListener(Check);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Check()
+    {
+        foreach (char num in userText.text)
+        {
+            if (!char.IsDigit(num))
+            {
+                Debug.Log("Wrong input! You must only enter a number value.");
+            }
+            else
+            {
+                if (level == 1 && userText.text == "22")
+                {
+                    Debug.Log("Well Done");
+                    level++;
+                    Debug.Log("Your current Level: " + level);
+                    newspaperObject.SetActive(false);
+                }
+                else if (level == 2 && userText.text == "22")
+                {
+
+                }
+            }
+        }
+
+    }
+}
--- IMSD_F20_Project_Two/Assets/Scripts/CloseNewspaper.cs
+++ IMSD_F20_Project_Two/Assets/Scripts/CloseNewspaper.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CloseNewspaper : MonoBehaviour
 {
-    float timer;
     public GameObject newspaper;
 
     public void ClosePaper()
